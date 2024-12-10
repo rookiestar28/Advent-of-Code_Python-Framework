97530314bb9bdcb346b27014cafbeb4e1d14d967
nitekat1124 <nitekat1124@gmail.com>
2024-12-10 13:35:09 +0800

    Day 10, more readable variable name

--- solutions/day10.py
+++ solutions/day10.py
@@ -6,22 +6,22 @@ class Solution(SolutionBase):
         rows, cols = len(_map), len(_map[0])
         dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-        targets = []
+        endpoints = []
         queue = [(i, j)]
 
         while queue:
             y, x = queue.pop(0)
-            s = _map[y][x]
-            _next = s + 1
+            curr = _map[y][x]
+            _next = curr + 1
             for dy, dx in dirs:
                 ny, nx = y + dy, x + dx
                 if 0 <= ny < rows and 0 <= nx < cols and _map[ny][nx] == _next:
                     if _next == 9:
-                        targets.append((ny, nx))
+                        endpoints.append((ny, nx))
                     else:
                         queue.append((ny, nx))
 
-        return targets
+        return endpoints
 
     def part1(self, data):
         _map = [list(map(int, line)) for line in data]
@@ -32,8 +32,8 @@ class Solution(SolutionBase):
         for i in range(rows):
             for j in range(cols):
                 if _map[i][j] == 0:
-                    targets = self.get_score(_map, i, j)
-                    score += len(set(targets))
+                    endpoints = self.get_score(_map, i, j)
+                    score += len(set(endpoints))
         return score
 
     def part2(self, data):
@@ -45,6 +45,6 @@ class Solution(SolutionBase):
         for i in range(rows):
             for j in range(cols):
                 if _map[i][j] == 0:
-                    targets = self.get_score(_map, i, j)
-                    score += len(targets)
+                    endpoints = self.get_score(_map, i, j)
+                    score += len(endpoints)
         return score
