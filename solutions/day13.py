6ef46df3dd726636dea630555fad61e1f54b0d00
nitekat1124 <nitekat1124@gmail.com>
2024-12-13 13:54:19 +0800

    Day 13, add a non-z3 version and it's way faster

--- solutions/day13.py
+++ solutions/day13.py
@@ -1,5 +1,5 @@
 from utils.solution_base import SolutionBase
-import z3
+# import z3
 
 
 class Solution(SolutionBase):
@@ -14,12 +14,20 @@ class Solution(SolutionBase):
             btn_b = [*map(lambda i: int(i[2:]), btn_b.split(": ")[1].split(", "))]
             prize = [*map(lambda i: int(i[2:]), prize.split(": ")[1].split(", "))]
 
+            """
             s = z3.Solver()
             times_a, times_b = z3.Ints("times_a times_b")
             s.add(btn_a[0] * times_a + btn_b[0] * times_b == prize[0])
             s.add(btn_a[1] * times_a + btn_b[1] * times_b == prize[1])
             if s.check() == z3.sat:
                 coins += s.model()[times_a].as_long() * 3 + s.model()[times_b].as_long()
+            """
+
+            times_b = (prize[1] * btn_a[0] - prize[0] * btn_a[1]) / (btn_b[1] * btn_a[0] - btn_b[0] * btn_a[1])
+            times_a = (prize[0] - btn_b[0] * times_b) / btn_a[0]
+
+            if 100 >= times_a >= 0 and 100 >= times_b >= 0 and times_a.is_integer() and times_b.is_integer():
+                coins += int(times_a) * 3 + int(times_b)
 
         return coins
 
@@ -34,11 +42,19 @@ class Solution(SolutionBase):
             btn_b = [*map(lambda i: int(i[2:]), btn_b.split(": ")[1].split(", "))]
             prize = [*map(lambda i: int(i[2:]) + 10000000000000, prize.split(": ")[1].split(", "))]
 
+            """
             s = z3.Solver()
             times_a, times_b = z3.Ints("times_a times_b")
             s.add(btn_a[0] * times_a + btn_b[0] * times_b == prize[0])
             s.add(btn_a[1] * times_a + btn_b[1] * times_b == prize[1])
             if s.check() == z3.sat:
                 coins += s.model()[times_a].as_long() * 3 + s.model()[times_b].as_long()
+            """
+
+            times_b = (prize[1] * btn_a[0] - prize[0] * btn_a[1]) / (btn_b[1] * btn_a[0] - btn_b[0] * btn_a[1])
+            times_a = (prize[0] - btn_b[0] * times_b) / btn_a[0]
+
+            if times_a.is_integer() and times_b.is_integer():
+                coins += int(times_a) * 3 + int(times_b)
 
         return coins
